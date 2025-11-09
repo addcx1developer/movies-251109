b52bb2eec73fd4f1e37f744a8a91a2a0e86b48ba
addcx1developer <addcx1developer@gmail.com>
2025-11-10 00:29:55 +0800

    Fix format for movies component

--- frontend/src/components/Movies.jsx
+++ frontend/src/components/Movies.jsx
@@ -34,15 +34,23 @@ function Movies() {
         <thead>
           <tr className="border-b bg-gray-100">
             <th className="px-2 py-2 font-semibold text-gray-700">Movie</th>
-            <th className="px-2 py-2 font-semibold text-gray-700">Release Date</th>
+            <th className="px-2 py-2 font-semibold text-gray-700">
+              Release Date
+            </th>
             <th className="px-2 py-2 font-semibold text-gray-700">Rating</th>
           </tr>
         </thead>
         <tbody>
           {movies.map((m) => (
-            <tr key={m.id} className="odd:bg-white even:bg-gray-50 hover:bg-gray-100">
+            <tr
+              key={m.id}
+              className="odd:bg-white even:bg-gray-50 hover:bg-gray-100"
+            >
               <td className="px-2 py-2">
-                <Link to={`/movies/${m.id}`} className="text-blue-600 hover:text-blue-800 font-medium">
+                <Link
+                  to={`/movies/${m.id}`}
+                  className="text-blue-600 hover:text-blue-800 font-medium"
+                >
                   {m.title}
                 </Link>
               </td>
