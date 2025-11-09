71375b7b3bca1141a5e26aab65450c173df7e0ed
addcx1developer <addcx1developer@gmail.com>
2025-11-10 00:57:46 +0800

    Add hide or show components when jwt existing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,30 @@
 import { Link, Outlet } from "react-router";
 
-import Home from "./components/Home";
+import { useState } from "react";
 
 function App() {
+  const [jwtToken, setJwtToken] = useState("");
+
   return (
     <div className="flex justify-center">
       <div className="container space-y-4">
         <header className="flex justify-between items-center py-4 border border-transparent border-b-gray-300">
           <h1 className="text-xl font-bold">Go Watch a Movie!</h1>
-          <Link
-            to="/login"
-            className="bg-green-700 hover:bg-green-600 text-white text-xs rounded-sm p-1 cursor-pointer transition duration-300 ease-in-out"
-          >
-            Login
-          </Link>
+          {jwtToken.trim() === "" ? (
+            <Link
+              to="/login"
+              className="bg-green-700 hover:bg-green-600 text-white text-xs rounded-sm p-1 cursor-pointer transition duration-300 ease-in-out"
+            >
+              Login
+            </Link>
+          ) : (
+            <a
+              href="#!"
+              className="bg-red-700 hover:bg-red-600 text-white text-xs rounded-sm p-1 cursor-pointer tansition duration-300 ease-in-out"
+            >
+              Logout
+            </a>
+          )}
         </header>
 
         <div className="grid grid-cols-3 gap-4">
@@ -43,30 +54,34 @@ function App() {
                   Genres
                 </Link>
               </li>
-              <li>
-                <Link
-                  to="/admin/movie/0"
-                  className="block p-2 bg-white hover:bg-gray-200 transition duration-300 ease-in-out"
-                >
-                  Add Movie
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/manage-catalogue"
-                  className="block p-2 bg-white hover:bg-gray-200 transition duration-300 ease-in-out"
-                >
-                  Manage Catalogue
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/graphql"
-                  className="block p-2 bg-white hover:bg-gray-200 transition duration-300 ease-in-out"
-                >
-                  GraphQL
-                </Link>
-              </li>
+              {jwtToken.trim() !== "" && (
+                <>
+                  <li>
+                    <Link
+                      to="/admin/movie/0"
+                      className="block p-2 bg-white hover:bg-gray-200 transition duration-300 ease-in-out"
+                    >
+                      Add Movie
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/manage-catalogue"
+                      className="block p-2 bg-white hover:bg-gray-200 transition duration-300 ease-in-out"
+                    >
+                      Manage Catalogue
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/graphql"
+                      className="block p-2 bg-white hover:bg-gray-200 transition duration-300 ease-in-out"
+                    >
+                      GraphQL
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
           </nav>
           <main className="col-span-2">
