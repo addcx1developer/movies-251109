343f54073fee4e234027f5583bf82397bcf43349
addcx1developer <addcx1developer@gmail.com>
2025-11-10 08:01:05 +0800

    Add get user by email

--- backend/cmd/api/handlers.go
+++ backend/cmd/api/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
 )
@@ -38,6 +39,12 @@ func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
 	err := app.readJSON(w, r, &requestPayload)
 	if err != nil {
 		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	user, err := app.DB.GetUserByEmail(requestPayload.Email)
+	if err != nil {
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
 		return
 	}
 
