6d5458c6819edf116d09298d52b87c2991725e4f
addcx1developer <addcx1developer@gmail.com>
2025-11-10 12:09:09 +0800

    Add authentication middleware

--- backend/cmd/api/middleware.go
+++ backend/cmd/api/middleware.go
@@ -2,17 +2,28 @@ package main
 
 import "net/http"
 
-func (app *application) enableCORS(h http.Handler) http.Handler {
+func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://*")
 
 		if r.Method != "OPTIONS" {
-			h.ServeHTTP(w, r)
+			next.ServeHTTP(w, r)
 			return
 		}
 
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, X-CSRF-Token, Authorization")
+	})
+}
+
+func (app *application) authRequired(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _, err := app.auth.GetTokenFromHeaderAndVerify(w, r)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
 	})
 }
