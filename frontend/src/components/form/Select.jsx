832bca96f2c78176ecf5a235c553ee638ec86c85
addcx1developer <addcx1developer@gmail.com>
2025-11-11 02:43:23 +0800

    Add select component

--- frontend/src/components/form/Select.jsx
+++ frontend/src/components/form/Select.jsx
@@ -1,20 +1,26 @@
-function TextArea(props) {
+function Select(props) {
   return (
     <div className="grid gap-2">
       <label htmlFor={props.name} className="font-semibold text-sm">
         {props.title}
       </label>
-      <textarea
+      <select
         id={props.name}
         name={props.name}
-        rows={props.rows}
         className="text-sm border border-gray-300 rounded-sm p-1"
         value={props.value}
         onChange={props.onChange}
-      />
+      >
+        <option value="">{props.placeHolder}</option>
+        {props.options.map((o) => (
+          <option key={o.id} value={o.id}>
+            {o.value}
+          </option>
+        ))}
+      </select>
       <div className={props.errorDiv}>{props.errorMsg}</div>
     </div>
   );
 }
 
-export default TextArea;
+export default Select;
